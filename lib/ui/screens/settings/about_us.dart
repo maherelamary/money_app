e9ca95f6db2afbe6067e756e8b33a266756267a6
Super Bekala <imac@imac.local>
2021-03-22 11:25:39 +0200

    add about us screen

--- lib/ui/screens/settings/about_us.dart
+++ lib/ui/screens/settings/about_us.dart
@@ -4,6 +4,91 @@ class AboutUsScreen extends StatelessWidget {
   static String routeName = '/about_us';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xffE84D8A),
+                Color(0xff7558AF),
+              ],
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 30.0,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'About Us',
+                    style: TextStyle(
+                      color: Color(0xffFEB326),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 35.0,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 60.0,
+                ),
+                _buildRowData(text: line1),
+                _buildRowData(text: line2),
+                _buildRowData(text: line1),
+                _buildRowData(text: line2),
+                _buildRowData(text: line2),
+                _buildRowData(text: line1),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
+
+  Widget _buildRowData({String text}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 8.0,
+      ),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 8.0,
+            ),
+            child: Icon(
+              Icons.circle,
+              //color: Color(0xffFEB326),
+              color: Color(0xff64c5eb).withOpacity(0.8),
+              size: 15.0,
+            ),
+          ),
+          Flexible(
+            child: Text(
+              text,
+              softWrap: true,
+              style: TextStyle(
+                color: Colors.white70,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  static String line1 =
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has";
+  static String line2 =
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the i";
 }
